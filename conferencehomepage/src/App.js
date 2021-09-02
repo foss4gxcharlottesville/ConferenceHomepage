af94172c917748851d48db8f07c4c6f54ff59f4b
epurpur <epurpur@gmail.com>
2021-09-02 10:48:21 -0400

    adding routes to app.js

--- conferencehomepage/src/App.js
+++ conferencehomepage/src/App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+/* Styles */
 import './App.css';
 
 /* Components */
@@ -7,25 +11,42 @@ import Container from 'react-bootstrap/Container';
 
 /* Pages */
 import Homepage from "./pages/Homepage";
+import Register from "./pages/Register";
 
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Container>
-        <Navbar.Brand href="#home">Foss4gx - Charlottesville</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        </Container>
-      </Navbar>
-      <Homepage></Homepage>
+    <Router>
 
-   </div>
+      <div className="App">
+        <Navbar bg="dark" variant="dark" id="header">
+          <Container>
+          <Navbar.Brand href="#home" id="brand">FOSS4Gx Charlottesville</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="home">Home</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
+            <Nav.Link href="registration">Registration</Nav.Link>
+            <Nav.Link href="blog">Blog</Nav.Link>
+            <Nav.Link href="sponsors">Sponsors</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
+          </Nav>
+          </Container>
+        </Navbar>
+
+        {/* Establishing routes to all endpoints */}
+        <Switch>
+          <Route exact path={["/", "/home"]}>
+            <Homepage />
+          </Route>
+          <Route exact path="/registration">
+            <Register />
+          </Route>
+        </Switch>
+        
+
+    </div>
+   </Router>
   );
 }
 
