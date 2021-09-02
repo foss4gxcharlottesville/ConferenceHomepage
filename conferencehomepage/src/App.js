9a04272df4fba04fe31182098cead4d002640a3c
epurpur <epurpur@gmail.com>
2021-09-02 10:13:04 -0400

    adding navbar to homepage

--- conferencehomepage/src/App.js
+++ conferencehomepage/src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 
+/* Components */
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+
+/* Pages */
+import Homepage from "./pages/Homepage";
+
+
 function App() {
+
   return (
     <div className="App">
-      <div></div>
-    </div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Foss4gx - Charlottesville</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+      <Homepage></Homepage>
+
+   </div>
   );
 }
 
---
+++ conferencehomepage/src/pages/Homepage/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/* CSS styles */
+import "./styles.css";
+
+const Homepage = () => {
+    return (
+        <div>
+            This is from the homepage
+            
+        </div>
+    )
+}
+
+export default Homepage;
