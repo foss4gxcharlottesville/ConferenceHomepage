28ff443a51d8a809b5d198796883a1aea018dc6f
epurpur <epurpur@gmail.com>
2021-09-02 14:37:38 -0400

    re-doing broken app

--- conferencehomepage/src/App.js
+++ conferencehomepage/src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
 /* Pages */
 import Homepage from "./pages/Homepage";
@@ -21,18 +22,19 @@ function App() {
 
       <div className="App">
         <Navbar bg="dark" variant="dark" id="header">
-          <Container>
-          <Navbar.Brand href="#home" id="brand">FOSS4Gx Charlottesville</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="home">Home</Nav.Link>
-            <Nav.Link href="about">About</Nav.Link>
-            <Nav.Link href="registration">Registration</Nav.Link>
-            <Nav.Link href="blog">Blog</Nav.Link>
-            <Nav.Link href="sponsors">Sponsors</Nav.Link>
-            <Nav.Link href="contact">Contact</Nav.Link>
-          </Nav>
-          </Container>
-        </Navbar>
+            <Container>
+            <Navbar.Brand href="#home" id="brand">FOSS4Gx Charlottesville</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="home">Home</Nav.Link>
+              <Nav.Link href="about">About</Nav.Link>
+              <Nav.Link href="registration">Registration</Nav.Link>
+              <Nav.Link href="blog">Blog</Nav.Link>
+              <Nav.Link href="sponsors">Sponsors</Nav.Link>
+              <Nav.Link href="contact">Contact</Nav.Link>
+            </Nav>
+            </Container>
+          </Navbar>
+      </div>
 
         {/* Establishing routes to all endpoints */}
         <Switch>
@@ -43,10 +45,7 @@ function App() {
             <Register />
           </Route>
         </Switch>
-        
-
-    </div>
-   </Router>
+    </Router>
   );
 }
 
